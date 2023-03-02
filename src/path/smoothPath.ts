d73e5280d8fe1c3afd88cbc66433813d32bc3746
Namide <damien.git@gmail.com>
2023-03-02 21:36:40 +0100

    Create Path class

--- src/path/smoothPath.ts
+++ src/path/smoothPath.ts
@@ -1,4 +1,46 @@
-export function smoothPath (path: number[][], { step = 1, keepStart = false, keepEnd = false, loop = false } = {}): number[][] {
+type Units = number[]
+
+export class Path {
+  public path: Units[]
+  public distance: number = 0
+  private _distances: number[] = []
+
+  constructor (path: Units[]) {
+    this.path = path
+    this._updateDistances()
+  }
+
+  _updateDistances () {
+    this._distances = []
+    this.distance = 0
+
+    for (let i = 1; i < this.path.length; i++) {
+      const prev = this.path[i - 1]
+      const next = this.path[i]
+      let distance = 0
+      for (let unit = 0; unit < prev.length; unit++) {
+        distance += (next[unit] - prev[unit]) ** 2 
+      }
+      distance = Math.sqrt(distance)
+      this.distance += distance
+      this._distances.push(distance)
+    }
+
+    this._distances = this._distances.map(dist => dist / this.distance)
+  }
+
+  getValue (t: number) {
+    return 1
+  }
+
+  smooth({ step = 1, keepStart = false, keepEnd = false, loop = false } = {}) {
+    this.path = _smoothPath(this.path, { step, keepStart, keepEnd, loop })
+    this._updateDistances()
+  }
+
+}
+
+export function _smoothPath (path: number[][], { step, keepStart, keepEnd, loop }): number[][] {
   const newPath: number[][] = keepStart ? [path[0]] : []
 
   for (let i = 0; i < path.length - (loop ? 0 : 1); i++) {
@@ -19,5 +61,5 @@ export function smoothPath (path: number[][], { step = 1, keepStart = false, kee
     newPath.push(path[path.length - 1])
   }
 
-  return step > -1 ? smoothPath(newPath, { step: step - 1, keepStart, keepEnd, loop }) : newPath
+  return step > -1 ? _smoothPath(newPath, { step: step - 1, keepStart, keepEnd, loop }) : newPath
 }
