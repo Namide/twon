05d2f2d4be748f75e06ffe96af7ba3abc311cd65
Namide <damien.git@gmail.com>
2023-03-03 00:50:06 +0100

    Add path

--- src/path/Path.ts
+++ src/path/Path.ts
@@ -3,11 +3,11 @@ type Units = number[]
 export class Path {
   public path: Units[]
   public distance: number = 0
+
   private _distances: number[] = []
 
   constructor (path: Units[]) {
     this.path = path
-    this._updateDistances()
   }
 
   _updateDistances () {
@@ -17,11 +17,13 @@ export class Path {
     for (let i = 1; i < this.path.length; i++) {
       const prev = this.path[i - 1]
       const next = this.path[i]
+
       let distance = 0
       for (let unit = 0; unit < prev.length; unit++) {
         distance += (next[unit] - prev[unit]) ** 2 
       }
       distance = Math.sqrt(distance)
+
       this.distance += distance
       this._distances.push(distance)
     }
@@ -30,17 +32,44 @@ export class Path {
   }
 
   getValue (t: number) {
-    return 1
+    if (t <= 0) {
+      return this.path[0]
+    }
+
+    if (t >= 1) {
+      return this.path[this.path.length - 1]
+    }
+
+    if (this._distances.length === 0) {
+      this._updateDistances()
+    }
+
+    for (let i = 0, start = 0; i < this._distances.length; i++) {
+      const current = this._distances[i]
+      if (t > start + current) {
+        const v = (t - start) / current
+        return _lerpUnit(this.path[i], this.path[i + 1], v)
+      }
+      start += current
+    }
+
+    return this.path[0]
   }
 
   smooth({ step = 1, keepStart = false, keepEnd = false, loop = false } = {}) {
     this.path = _smoothPath(this.path, { step, keepStart, keepEnd, loop })
-    this._updateDistances()
   }
-
 }
 
-export function _smoothPath (path: number[][], { step, keepStart, keepEnd, loop }): number[][] {
+function _lerpUnit (from: Units, to: Units, t: number) {
+  const values: Units = []
+  for (let i = 0; i < from.length; i++) {
+    values.push((to[i] - from[i]) * t + from[i])
+  }
+  return values
+}
+
+function _smoothPath (path: number[][], { step, keepStart, keepEnd, loop }): number[][] {
   const newPath: number[][] = keepStart ? [path[0]] : []
 
   for (let i = 0; i < path.length - (loop ? 0 : 1); i++) {
